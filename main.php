a60c8c3213515f71be053f855c4237e379a6144a
Rūta R <rutar4t@gmail.com>
2018-12-03 17:21:06 +0200

    pridejau nav bar

--- main.php
+++ main.php
@@ -1,7 +1,38 @@
 <?php 
-
-
 echo "main page";
-
-
 ?>
+
+<!DOCTYPE HTML>
+<html lang="lt">
+	<head>
+		<title>Rutos ir Agnes kursinis projektas</title>
+        <!-- <meta charset="utf-8"> -->
+        <!-- <link rel="stylesheet" type="text/css" href="style/style.css"> -->
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	</head>
+	<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Rutos ir Agnes kursinis projektas</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Projektai</a></li>
+      <li><a href="#">Klientai</a></li>
+      <li><a href="#">Sąskaitos</a></li>
+    </ul>
+  </div>
+</nav>
+<div class="container">
+
+</div>
+        <div>
+        </div>
+    </body>
+</html>
